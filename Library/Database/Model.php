a77fbff5935a63a9aaf6857345120f09c3a8a87d
givi odikadze <guivio_147@hotmail.com>
2015-03-06 02:36:52 -0500

    added magic get set to model

--- Library/Database/Model.php
+++ Library/Database/Model.php
@@ -6,13 +6,38 @@ class Model
 {
     protected $dao;
     protected $blueprint;
+    protected $vars = array();
 
-    public function __construct()
+    public function __construct(array $data = array())
     {
         $this->dao = DB::dao();
 
         $modelName = get_called_class();
         $modelName = strtolower(substr(get_called_class(), strrpos($modelName, '\\') + 1));
         $this->blueprint = DB::getBlueprint($modelName);
+
+        if (!empty($data)) {
+            foreach ($data as $var => $value) {
+                $this->__set($var, $value);
+            }
+        }
+    }
+
+    public function __set($var, $value) {
+        $this->vars[$var] = $value;
+    }
+
+    public function __get($var) {
+        if (isset($this->vars[$var]))
+            return $this->vars[$var];
+    }
+
+    /* MOVE METHODS BELOW OUT OF HERE */
+
+    public function save()
+    {
+        // if is new blablabla
+        // make a common query interface
+        // which is also accessible from db class
     }
 }
