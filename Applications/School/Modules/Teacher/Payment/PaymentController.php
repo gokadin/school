6a783a92ef73738498a87a4ef53685cdcaf98479
givi odikadze <guivio_147@hotmail.com>
2015-03-29 17:49:23 -0400

    started frontend design

--- Applications/School/Modules/Teacher/Payment/PaymentController.php
+++ Applications/School/Modules/Teacher/Payment/PaymentController.php
@@ -11,6 +11,22 @@ class PaymentController extends BackController
 
     public function test()
     {
+        \Stripe\Stripe::setApiKey("sk_test_dVsKknKBGTxmMO87Aah9MBzn");
 
+// Get the credit card details submitted by the form
+        $token = $_POST['stripeToken'];
+
+// Create the charge on Stripe's servers - this will charge the user's card
+        try {
+            $charge = \Stripe\Charge::create(array(
+                    "amount" => 1000, // amount in cents, again
+                    "currency" => "cad",
+                    "source" => $token,
+                    "description" => "payinguser@example.com")
+            );
+            echo 'success';
+        } catch(\Stripe\Error\Card $e) {
+            echo 'card declined';
+        }
     }
 }
