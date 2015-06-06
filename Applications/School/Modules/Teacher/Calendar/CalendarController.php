0efda26aa0586e4045c4159632a50458294ee959
givi odikadze <guivio_147@hotmail.com>
2015-06-06 17:13:54 -0400

    added student tag input

--- Applications/School/Modules/Teacher/Calendar/CalendarController.php
+++ Applications/School/Modules/Teacher/Calendar/CalendarController.php
@@ -1,11 +1,18 @@
 <?php namespace Applications\School\Modules\Teacher\Calendar;
 
 use Library\BackController;
+use Library\Facades\Page;
 
 class CalendarController extends BackController
 {
 	public function index()
 	{
+		$studentNames = array();
+		foreach ($this->currentUser->students() as $student)
+		{
+			$studentNames[] = $student->name();
+		}
 		
+		Page::add('studentNames', $studentNames);
 	}
 }
