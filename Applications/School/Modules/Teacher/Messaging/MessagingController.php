d7274fe3b84445b14cd91c6dad6805d600d06130
Givi Odikadze <guivio_147@hotmail.com>
2015-07-01 22:31:08 -0400

    working on messaging

--- Applications/School/Modules/Teacher/Messaging/MessagingController.php
+++ Applications/School/Modules/Teacher/Messaging/MessagingController.php
@@ -1,12 +1,13 @@
 <?php namespace Applications\School\Modules\Teacher\Messaging;
 
 use Library\BackController;
+use Library\Facades\Page;
 
 class MessagingController extends BackController
 {
 	public function index()
 	{
-		
+		Page::add('students', $this->currentUser->students());
 	}
 
     public function test()
