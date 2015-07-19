482d0ba4e497864080d9953da3248a74f0984467
Givi Odikadze <guivio_147@hotmail.com>
2015-07-19 16:34:52 -0400

    added test unit test

--- Tests/ApplicationTest/App/Http/Controllers/Frontend/AccountControllerTest.php
+++ Tests/ApplicationTest/App/Http/Controllers/Frontend/AccountControllerTest.php
@@ -2,13 +2,25 @@
 
 namespace Tests\ApplicationTest\App\Http\Controllers\Frontend;
 
+use App\Http\Controllers\Frontend\AccountController;
 use Models\Teacher;
 use Tests\FrameworkTest\BaseTest;
+use Library\Database\Factory;
 
 class AccountControllerTest extends BaseTest
 {
     public function testLogin()
     {
-        $this->assertTrue(true);
+        // Arrange
+        $controller = new AccountController();
+        Factory::of(Teacher::class)->create(1, [
+            'email' => 'a@b.com',
+            'password' => md5('admin')
+        ]);
+
+        // Act
+
+        // Assert
+        $this->assertEquals('a@b.com', Teacher::all()->first()->email);
     }
 }
