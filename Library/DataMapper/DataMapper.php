86cb19bd3a896120353cef5414a1d0d900be56c2
givi odikadze <guivio_147@hotmail.com>
2015-09-15 15:09:38 -0400

    started fresh datamapper branch

---
+++ Library/DataMapper/DataMapper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Library\DataMapper;
+
+class DataMapper
+{
+    public function __construct($config)
+    {
+
+    }
+
+    public function find($class, $id)
+    {
+
+    }
+
+    public function findOrFail($class, $id)
+    {
+
+    }
+
+    public function persist($object)
+    {
+
+    }
+
+    public function update($object)
+    {
+
+    }
+
+    public function delete($classOrObject, $id = 0)
+    {
+
+    }
+
+    public function flush()
+    {
+
+    }
+
+    public function createQuery($str)
+    {
+
+    }
+
+    public function getQueryBuilder()
+    {
+        
+    }
+}
