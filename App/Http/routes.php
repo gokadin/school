660a701a62fdcc107810ac5f2a68b59c7bacfcba
givi odikadze <guivio_147@hotmail.com>
2016-04-16 12:55:30 -0400

    removed more legacy code

--- App/Http/routes.php
+++ App/Http/routes.php
@@ -53,36 +53,6 @@ $router->group(['namespace' => 'Api', 'prefix' => '/api', 'as' => 'api', 'middle
     });
 });
 
-//$router->group(['namespace' => 'Api', 'prefix' => '/api', 'as' => 'api', 'middleware' => 'VerifyCsrfToken'], function($router) {
-//    $router->group(['namespace' => 'School', 'prefix' => '/school', 'as' => 'school', 'middleware' => 'VerifyAuthentication'], function($router) {
-//
-//        $router->group(['namespace' => 'Teacher', 'prefix' => '/teacher', 'as' =>'teacher'], function($router)
-//        {
-//            $router->group(['prefix' => '/students', 'as' => 'student'], function($router) {
-//                $router->get('/new-students', 'StudentController@newStudents');
-//                $router->post('/', 'StudentController@index');
-//                $router->post('/from-ids', 'StudentController@fromIds');
-//                $router->post('/search', 'StudentController@search');
-//                $router->post('/{id}/lessons/range', 'StudentController@lessonRange');
-//            });
-//
-//            $router->group(['prefix' => '/events', 'as' => 'event'], function($router)
-//            {
-//                //$router->get('/upcoming-events', 'EventController@upcomingEvents');
-//                $router->post('/', 'EventController@create');
-//                //$router->post('/range', 'EventController@range');
-//                $router->put('/change-date', 'EventController@changeDate');
-//                $router->delete('/{id}', 'EventController@destroy');
-//                $router->put('/{eventId}/lessons/{lessonId}/attendance', 'EventController@updateLessonAttendance');
-//            });
-//
-//            $router->get('/get-registration-form', 'SettingController@getRegistration');
-//            $router->post('/update-registration-form', 'SettingController@updateRegistrationForm');
-//        });
-//    });
-//});
-
-
 $router->catchAll(function() {
     return new \Library\Http\View('school.index');
 });
