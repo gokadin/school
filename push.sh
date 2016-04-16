3b626d01608c9a70008d11167de00fbddd77107c
givi odikadze <guivio_147@hotmail.com>
2016-04-16 12:29:29 -0400

    test

--- push.sh
+++ push.sh
@@ -5,4 +5,11 @@ git add -A .
 read -p "Message: " message
 git commit -m $message
 
-git push origin master
+if [ "$1" != "" ]
+then branch="$1"
+else branch="master"
+fi
+
+echo $branch
+
+#git push origin $branch
